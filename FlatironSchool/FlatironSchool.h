1f5e47f6779c564af3d05884e20c7b055d1f00ec
Matt Isaacs <matt@ustwo.com>
2016-10-30 14:45:59 -0400

    Initial Commit

---
+++ FlatironSchool/FlatironSchool.h
@@ -0,0 +1,19 @@
+//
+//  FlatironSchool.h
+//  FlatironSchool
+//
+//  Created by Matt Isaacs on 10/30/16.
+//  Copyright © 2016 Ustwo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FlatironSchool.
+FOUNDATION_EXPORT double FlatironSchoolVersionNumber;
+
+//! Project version string for FlatironSchool.
+FOUNDATION_EXPORT const unsigned char FlatironSchoolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlatironSchool/PublicHeader.h>
+
+
